bf69ca3232ece893e9dc411f6b4741ea4e5437b6
dtv <dien.truong@axonactive.vn>
2025-05-27 09:16:12 +0700

    fix: migration css corrupt

--- eventastic-horizon-next/app/page.tsx
+++ eventastic-horizon-next/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import MainLayout from "./components/layout/MainLayout";
 import Hero from "./components/home/Hero";
 import Features from "./components/home/Features";
